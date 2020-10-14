c646fd61437b2136980d91ba213baed404980a5d
Godoy Gabriel <58715211+GodoyGabriel@users.noreply.github.com>
2020-10-14 01:38:12 -0300

    Add Jumbo component and fixes

---
+++ src/components/Jumbo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyledJumbo } from '../styles/components';
+
+export default function Jumbo() {
+  return (
+    <StyledJumbo>
+          <div>
+        <h2>Haciendo pruebas</h2>
+        <small>Piiezas elegantes</small>
+      </div>
+    </StyledJumbo>
+  )
+}
