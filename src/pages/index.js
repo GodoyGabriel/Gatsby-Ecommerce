ac61904bc5573ddc004e0bc88bde32a0d7d0157e
Godoy Gabriel <58715211+GodoyGabriel@users.noreply.github.com>
2020-10-16 02:26:45 -0300

    Styled button in IndexPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
-import { Jumbo } from "../components"
-import { SEO } from "../components"
+import React from "react";
+import { Link, graphql } from "gatsby";
+import { Jumbo } from "../components";
+import { SEO } from "../components";
+import styled from 'styled-components';
 
 export const query = graphql`
   query GET_SITEMETADATA {
@@ -18,15 +19,24 @@ export const query = graphql`
   }
 `
 
+const Button = styled.button`
+  width: 8rem;
+  background-color: #98ca3f;
+  border: none;
+  border-radius: 10px;
+  color: ${props => props.color};
+  &:hover{
+    transform: scale(1.4);
+  }
+`
+
 const IndexPage = ({ data }) => {
   const siteMetadata = data.allSite.edges[0].node.siteMetadata;
   return (
     <>
     <SEO title="Home" />
     <Jumbo siteMetaData={siteMetadata}/>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <Button color="gray">Comprar</Button>
     <Link to="/ThankYou">Go to Thank You</Link>
   </>
   )
